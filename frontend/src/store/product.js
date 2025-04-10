c8acd28036e7b08a1bff8c3db3813e15e4d8ea47
AzulaFire <jhornjr@gmail.com>
2025-04-10 13:09:09 +0900

    updated api path

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -7,7 +7,7 @@ export const useProductStore = create((set) => ({
     if (!newProduct.name || !newProduct.image || !newProduct.price) {
       return { success: false, message: 'Please fill in all fields.' };
     }
-    const res = await fetch('/api/products', {
+    const res = await fetch('https://mern-store-eta.vercel.app//api/products', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -19,14 +19,17 @@ export const useProductStore = create((set) => ({
     return { success: true, message: 'Product created successfully' };
   },
   fetchProducts: async () => {
-    const res = await fetch('/api/products');
+    const res = await fetch('https://mern-store-eta.vercel.app//api/products');
     const data = await res.json();
     set({ products: data.data });
   },
   deleteProduct: async (pid) => {
-    const res = await fetch(`/api/products/${pid}`, {
-      method: 'DELETE',
-    });
+    const res = await fetch(
+      `https://mern-store-eta.vercel.app//api/products/${pid}`,
+      {
+        method: 'DELETE',
+      }
+    );
     const data = await res.json();
     if (!data.success) return { success: false, message: data.message };
 
@@ -37,13 +40,16 @@ export const useProductStore = create((set) => ({
     return { success: true, message: data.message };
   },
   updateProduct: async (pid, updatedProduct) => {
-    const res = await fetch(`/api/products/${pid}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(updatedProduct),
-    });
+    const res = await fetch(
+      `https://mern-store-eta.vercel.app//api/products/${pid}`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedProduct),
+      }
+    );
     const data = await res.json();
     if (!data.success) return { success: false, message: data.message };
 
