3103153c9687a94c6c283f6d7bf433fa20bf60c7
AzulaFire <jhornjr@gmail.com>
2025-04-10 17:31:15 +0900

    Updated store name

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -3,6 +3,7 @@ import { create } from 'zustand';
 export const useProductStore = create((set) => ({
   products: [],
   setProducts: (products) => set({ products }),
+
   createProduct: async (newProduct) => {
     if (!newProduct.name || !newProduct.image || !newProduct.price) {
       return { success: false, message: 'Please fill in all fields.' };
@@ -18,11 +19,13 @@ export const useProductStore = create((set) => ({
     set((state) => ({ products: [...state.products, data] }));
     return { success: true, message: 'Product created successfully' };
   },
+
   fetchProducts: async () => {
     const res = await fetch('/api/products');
     const data = await res.json();
     set({ products: data });
   },
+
   deleteProduct: async (pid) => {
     const res = await fetch(`/api/products?id=${pid}`, {
       method: 'DELETE',
@@ -35,6 +38,7 @@ export const useProductStore = create((set) => ({
     }));
     return { success: true, message: data.message };
   },
+
   updateProduct: async (pid, updatedProduct) => {
     const res = await fetch(`/api/products?id=${pid}`, {
       method: 'PUT',
